2ce9acc2a70fd2498d8778fd4dd4d0105221bbd1
trevorborden <trevor.byron@procore.com>
2021-02-03 15:06:47 -0800

    styling updates

--- components0/search/index.jsx
+++ components0/search/index.jsx
@@ -1,29 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <!-- <link rel="stylesheet" href="INVITED/icon-font.INVITED"> -->
-  <link rel="stylesheet" href="css/style.css">
-  <link rel="shortcut icon" type="image/png" href="img/favicon.png">
-  <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Round" rel="stylesheet">
-  <link rel="preconnect" href="https://fonts.gstatic.com">
-<link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+import React from "react"
 
 
+export const Search=()=> {
 
-  <title>Construction Network </title>
-</head>
-
-<body class="landing-page-body">
-
-
-  <!-- Container holding the top bars so that they can be positioned together and fixed to the top -->
-
-  <!-- Site Header-->
-
-
+return(
+  <div>
   <div class="site-header">
     <div class="site-header_logo">
       <img class="site-header_logo_img center--horizontal" src="img/procorelogo.svg" alt="">
@@ -43,17 +24,26 @@
   </div>
 
   <div class="construction-network-search reveal">
-
-    <a href="index.html" class="site-header_logo_small center--horizontal"></a>
     <a href="index.html"> <img class="site-header_logo_img revealing-logo  center--horizontal" src="img/procorelogo.svg" alt=""></a>
 
     <form>
-  <div class="search-location">
-   <input type="search" placeholder="Search by trade or business (e.g ‘Plumbing’ or ‘Concrete’) ">
-   <input class="zip" type="text" name="" value="Santa Barbara, CA">
-  <div class="search-trigger"> </div>
-  </div>
-
+      <!--
+                <select id="search-selector" name="search-selector">
+                    <option value="Companies">Companies</option>
+                    <option value="Projects">Projects</option>
+                </select>
+-->
+      <input type="search" placeholder="Search by trade or business  ">
+      <select id="search-selector" name="search-selector">
+        <option value="Commercial">Commercial</option>
+        <option value="Infrastructure">Healthcare</option>
+        <option value="Infrastructure">Government</option>
+        <option value="Infrastructure">Residential - Single Family</option>
+        <option value="Infrastructure">Residential - Multi Family</option>
+        <option value="Infrastructure">Infrastructure</option>
+        <option value="Industrial">Industrial & Energy</option>
+      </select>
+      <input class="zip" type="text" name="" value="93103">
     </form>
 
     <button class="tertiary  revealing-button loading-bois">
@@ -61,24 +51,45 @@
           <circle class="path" cx="24" cy="24" r="20" fill="none" stroke-width="8"></circle>
         </svg></spinner><label> List Your Business</label>
     </button>
+
+
   </div>
 
 
 
   <section class="search-hero">
-    <div class="construction-network-search">
-      <div class="search-location">
-       <input type="search" placeholder="Search by trade or business (e.g ‘Plumbing’ or ‘Concrete’) ">
-       <input class="zip" type="text" name="" value="Santa Barbara, CA">
-      <div class="search-trigger"> </div>
-      </div>
-    </div>
-    <h1>Find Everyone in Construction. Right Here.</h1>
+    <h1>All the Construction Pros on one platform.</h1>
     <h2>152,<span class="Single">639</span> contractors and counting. </h2>
 
     <h2></h2>
+    <div class="construction-network-search">
+      <form>
+        <input type="search" placeholder="Search by trade or business name  ">
+        <select id="search-selector" name="search-selector">
+          <option value="Commercial">Commercial</option>
+          <option value="Infrastructure">Healthcare</option>
+          <option value="Infrastructure">Government</option>
+          <option value="Infrastructure">Residential - Single Family</option>
+          <option value="Infrastructure">Residential - Multi Family</option>
+          <option value="Infrastructure">Infrastructure</option>
+          <option value="Industrial">Industrial & Energy</option>
+        </select>
+        <input class="zip" type="text" name="" value="93103">
 
 
+        <button class="">
+          <a href="SRP-Page--Electricians.html">
+            <spinner><svg class="spinner" viewBox="0 0 48 48">
+                <circle class="path" cx="24" cy="24" r="20" fill="none" stroke-width="8"></circle>
+              </svg></spinner><label>Search</label>
+          </a>
+        </button>
+
+
+
+
+      </form>
+    </div>
   </section>
 
   <section class="content landing-page-section">
@@ -88,64 +99,90 @@
     </p>
   </section>
 
-  <section class="recent-projects landing-page-section">
+  <section class="top-performing-contrators landing-page-section">
 
-      <h2> Browse building professionals in Santa Barbara by company type</h2>
-      <div class="flex-box flex-start company-types">
-        <div class="company-type-option">
-          <div class="company-type__image general-contractor"></div>
-          <div class="company-type__title">
-            <span>General Contractors</span>
-          </div>
+    <h2> Top Contractors in Santa Barbara, CA </h2>
+
+    <ul class="suggested-cities">
+
+      <li> <a href="#">Ventura</a></li>
+      <li><a href="#">Los Angeles</a></li>
+      <li><a href="#">San Francisco</a></li>
+      <li><a href="#">Chicago</a></li>
+      <li><a href="#">Orlando</a></li>
+      <li><a href="#">All Cities</a></li>
+    </ul>
+
+    <div class="similar-businesses">
+      <div class="similar-businesses__business">
+        <div class="business__logo logo_unander">
+        </div>
+        <div class="business__name">
+          <span> Unander Construction</span>
+        </div>
+        <div class="business__activity">
+          <span class="count">7</span> <span> Active Procore Projects</span>
         </div>
 
-        <div class="company-type-option">
-          <div class="company-type__image specialty-contractor"></div>
-          <div class="company-type__title">
-            <span>Specialty Contractors</span>
-          </div>
-        </div>
-
-
-        <div class="company-type-option">
-          <div class="company-type__image Architects-Designers"></div>
-          <div class="company-type__title">
-            <span>Architects & Designers</span>
-          </div>
-        </div>
-
-        <div class="company-type-option">
-          <div class="company-type__image suppliers-vendors"></div>
-          <div class="company-type__title">
-            <span>Suppliers & Vendors </span>
-          </div>
-        </div>
-
-        <div class="company-type-option">
-          <div class="company-type__image general-contractor"></div>
-          <div class="company-type__title">
-            <span>Engineers </span>
-          </div>
-        </div>
-
-        <div class="company-type-option">
-          <div class="company-type__image general-contractor"></div>
-          <div class="company-type__title">
-            <span>Construction Managers </span>
-          </div>
-        </div>
-
-        <div class="company-type-option">
-          <div class="company-type__image general-contractor"></div>
-          <div class="company-type__title">
-            <span>Realestate Developers </span>
-          </div>
-        </div>
       </div>
 
+      <div class="similar-businesses__business">
+        <a href="business-profile--DE.html">
+          <div class="business__logo logo_DE">
+          </div>
+          <div class="business__name">
+            <span> David Larson Electric</span>
+          </div>
+          <div class="business__activity">
+            <span class="count"> 1</span> <span> Active Procore Projects</span>
+          </div>
+
+      </div>
+
+
+      <div class="similar-businesses__business">
+        <a href="business-profile--AC.html">
+          <div class="business__logo logo_AC">
+          </div>
+          <div class="business__name">
+            <span> AC Electric</span>
+          </div>
+          <div class="business__activity">
+            <span class="count"> 6</span> <span> Active Procore Projects</span>
+          </div>
+        </a>
+      </div>
+
+      <div class="similar-businesses__business">
+        <div class="business__logo logo_turner">
+        </div>
+        <div class="business__name">
+          <span> Turner Construction</span>
+        </div>
+        <div class="business__activity">
+          <span class="count"> 2</span> <span> Active Procore Projects</span>
+        </div>
+
+      </div>
+      <a href="business-profile--JR.html">
+        <div class="similar-businesses__business">
+
+          <div class="business__logo logo_jr">
+          </div>
+          <div class="business__name">
+            <span> JR Electric</span>
+          </div>
+          <div class="business__activity">
+            <span class="count"> 3</span> <span> Active Procore Projects</span>
+          </div>
+
+        </div>
+      </a>
+
+
+    </div>
+
   </section>
-
-
 
 
   <section class="recent-projects landing-page-section">
@@ -305,7 +342,6 @@
           </svg></spinner><label> View all</label>
       </button></a>
   </section>
-
   <section class="recent-projects landing-page-section">
     <h2> Browse companies in Santa Barbara by trade</h2>
     <div class="flex-box space-between flex-wrap">
@@ -374,91 +410,6 @@
         </svg></spinner><label> View all trades</label>
     </button></a>
   </section>
-  <section class="top-performing-contrators landing-page-section">
-
-    <h2> Top Contractors in Santa Barbara, CA </h2>
-
-    <ul class="suggested-cities">
-
-      <li> <a href="#">Ventura</a></li>
-      <li><a href="#">Los Angeles</a></li>
-      <li><a href="#">San Francisco</a></li>
-      <li><a href="#">Chicago</a></li>
-      <li><a href="#">Orlando</a></li>
-      <li><a href="#">All Cities</a></li>
-    </ul>
-
-    <div class="similar-businesses">
-      <div class="similar-businesses__business">
-        <div class="business__logo logo_unander">
-        </div>
-        <div class="business__name">
-          <span> Unander Construction</span>
-        </div>
-        <div class="business__activity">
-          <span class="count">7</span> <span> Active Procore Projects</span>
-        </div>
-
-      </div>
-
-      <div class="similar-businesses__business">
-        <a href="business-profile--DE.html">
-          <div class="business__logo logo_DE">
-          </div>
-          <div class="business__name">
-            <span> David Larson Electric</span>
-          </div>
-          <div class="business__activity">
-            <span class="count"> 1</span> <span> Active Procore Projects</span>
-          </div>
-
-      </div>
-
-
-      <div class="similar-businesses__business">
-        <a href="business-profile--AC.html">
-          <div class="business__logo logo_AC">
-          </div>
-          <div class="business__name">
-            <span> AC Electric</span>
-          </div>
-          <div class="business__activity">
-            <span class="count"> 6</span> <span> Active Procore Projects</span>
-          </div>
-        </a>
-      </div>
-
-      <div class="similar-businesses__business">
-        <div class="business__logo logo_turner">
-        </div>
-        <div class="business__name">
-          <span> Turner Construction</span>
-        </div>
-        <div class="business__activity">
-          <span class="count"> 2</span> <span> Active Procore Projects</span>
-        </div>
-
-      </div>
-      <a href="business-profile--JR.html">
-        <div class="similar-businesses__business">
-
-          <div class="business__logo logo_jr">
-          </div>
-          <div class="business__name">
-            <span> JR Electric</span>
-          </div>
-          <div class="business__activity">
-            <span class="count"> 3</span> <span> Active Procore Projects</span>
-          </div>
-
-        </div>
-      </a>
-
-
-    </div>
-
-  </section>
-
 
 
 
@@ -630,43 +581,41 @@
 
   </section>
 
+  <section class="company-type">
+    <div class="landing-page-section">
+      <h2> Browse by company Type</h2>
+      <div class="flex-box flex-start">
+        <div class="company-type-option">
+          <div class="company-type__image general-contractor"></div>
+          <div class="company-type__title">
+            <span>General Contractors</span>
+          </div>
+        </div>
+
+        <div class="company-type-option">
+          <div class="company-type__image specialty-contractor"></div>
+          <div class="company-type__title">
+            <span>Specialty Contractors</span>
+          </div>
+        </div>
 
 
+        <div class="company-type-option">
+          <div class="company-type__image Architects-Designers"></div>
+          <div class="company-type__title">
+            <span>Architects & Designers</span>
+          </div>
+        </div>
 
+        <div class="company-type-option">
+          <div class="company-type__image suppliers-vendors"></div>
+          <div class="company-type__title">
+            <span>Suppliers & Vendors </span>
+          </div>
+        </div>
+      </div>
+    </div>
 
-
-
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-
-  <script>
-    $({
-      Counter: 600
-    }).animate({
-      Counter: $('.Single').text()
-    }, {
-      duration: 20000,
-      easing: 'linear',
-      step: function() {
-        $('.Single').text(Math.ceil(this.Counter));
-      }
-    });
-
-
-    $(function(){
-    $(window).scroll(function() {
-        if ($(this).scrollTop() > 500) {
-            $('.reveal').css('visibility','visible');
-            $('.reveal').slideDown('fast');
-        }
-        else {
-            $('.reveal').slideUp('fast');
-        }
-    });
-});
-  </script>
-
-</body>
-
-
-
-</html>
+  </section> </div>
+)
+}
